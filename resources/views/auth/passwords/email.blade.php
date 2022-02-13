0b54b6966d8143d580ddcb6592a40d58325fc963
KsKYt <ocul_1@yahoo.co.jp>
2022-02-13 16:08:53 +0900

    update

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,127 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('パスワードを再設定する。') }}</div>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" type="text/css" href="../css/reset.css">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Eメール') }}</label>
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+<body>
+<div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+     
+               <div class="app-head">
+               <div class="nav-back">
+                 
+                            <span class="navbar-toggler-icon"></span>
+                            
+                   
+                            <a class="back linkb bgleft" href="{{ url('/top1') }}">
+                                <span>{{ config('戻る', '戻る') }}</span>
+                            </a>
+                   </div>
+                     
+        
+                    
+    
+           
+               </div>
+                                   
+                        
+                        
+                
+                </div>
+            </div>
+        </nav>
+
+            
+            <main class="py-4">
+        
+
+
+         
+          
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <p class="login-top">
+                            パスワードを再設定する。
+</p>
+
+                   
+                         <div class="email-content
+                         ">
+                                @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                                @endif
+    
+                                <form method="POST" action="{{ route('password.email') }}">
+                                    @csrf
+    
+                                    <div class="login-email">
+                                        <div class="login-email-in">
+                                            <label for="email" class="login-titl"><p>Eメール</p></label>
+    
+                                           
+                                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+    
+                                                @error('email')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                                @enderror
+                                           
+                                        </div>
+                                    </div>
+    
+
+                            
+                    
+                          
+                                  
+                            
+                              
+            
 
                         <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('送信') }}
-                                </button>
-                            </div>
+                                    <div class="login-btn">
+                                        <div class="login-submit">
+                                            <button type="submit" class="login-btn-in bgleft">
+                                             <span>   {{ __('送信') }}</span>
+                                            </button>
+                                        </div>
+                                    </div>
+                         </div>
+                            </form>
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
+
+
+    
+        </main>
     </div>
-</div>
-@endsection
+</body>
+
+</html>
